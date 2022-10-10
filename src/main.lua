49eaf738aacf9717c761efa3ff9e9aa0dc1da643
Calvin Josenhans <60663079+HalflingHelper@users.noreply.github.com>
2022-10-10 18:11:02 -0400

    Window rendering is slightly more dynamic

--- src/main.lua
+++ src/main.lua
@@ -2,39 +2,24 @@
 require 'mandlebrot'
 
 --Eventually add a slider to handle iterations cap, otherwise we just do this
-MAX_ITERATIONS = 20
+MAX_ITERATIONS = 40
 THRESHHOLD = 2
 
-RENDER_WIDTH, RENDER_HEIGHT = 256, 256
+RENDER_WIDTH, RENDER_HEIGHT = 512, 512
 
 --{xmin, xmax, ymin, ymax}
-GRAPH_BOUNDS = { -3, 1, -2, 2 }
+GRAPH_BOUNDS = { -2.25, 1.75, -2, 2 }
 
 ZOOM_FACTOR = 2
 
 
 
 function love.load()
-    image_data = love.image.newImageData(RENDER_WIDTH, RENDER_HEIGHT, "r8")
+    love.window.setMode(RENDER_WIDTH, RENDER_HEIGHT)
+    image_data = love.image.newImageData(RENDER_WIDTH, RENDER_HEIGHT)
     image = love.graphics.newImage(image_data)
 
-    for x = 0, RENDER_WIDTH-1 do
-        for y = 0, RENDER_HEIGHT-1 do
-            local cx = x/64 - 3
-            local cy = y/64 - 2
-
-            local d = Eval_Mandlebrot(cx, cy)
-
-            if (d == -1) then
-                --It's black
-                image_data:setPixel(x, y, 0, 0, 0, 0)
-            else
-                image_data:setPixel(x, y, d/MAX_ITERATIONS, d/MAX_ITERATIONS, d/MAX_ITERATIONS, 1)
-            end
-        end
-    end
-
-    image:replacePixels(image_data)
+    update_image_data()
 end
 
 function love.draw()
@@ -46,4 +31,36 @@ function love.mousepressed(x, y, button)
         --Update bounds based on mouse position
         --Rerender the set
     end
+end
+
+--Returns an rgb color based on a curve between v and max
+function determine_color(v, max)
+    return {1, 1, 1}
+end
+
+--Updates image data based on the global params
+function update_image_data()
+    local cx = GRAPH_BOUNDS[1]
+    local cy = GRAPH_BOUNDS[3]
+    local dcx = (GRAPH_BOUNDS[2]-GRAPH_BOUNDS[1])/RENDER_WIDTH
+    local dcy = (GRAPH_BOUNDS[4]-GRAPH_BOUNDS[3])/RENDER_HEIGHT
+
+    for x = 0, RENDER_WIDTH-1 do
+        cx = cx + dcx
+        cy = GRAPH_BOUNDS[3]
+
+        for y = 0, RENDER_HEIGHT-1 do
+            cy = cy + dcy
+            local d = Eval_Mandlebrot(cx, cy)
+
+            if (d == -1) then
+                --It's black
+                image_data:setPixel(x, y, 0, 0, 0, 0)
+            else
+                image_data:setPixel(x, y, determine_color(d, MAX_ITERATIONS), 1)
+            end
+        end
+    end
+
+    image:replacePixels(image_data)
 end
